1b2017bedc4da0bcd530404793ae2f3cb3469ba0
kevin-joshua <kevinajijoshua@gmail.com>
2026-04-15 19:21:49 +0530

    / endpoint

--- server.js
+++ server.js
@@ -20,6 +20,10 @@ app.use(cors());
 app.use(express.json({ limit: '10mb' }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
   max: 20,
