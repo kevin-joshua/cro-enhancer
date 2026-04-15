1c8ee7c284e23b9908f6defe578182c470a76204
kevin-joshua <kevinajijoshua@gmail.com>
2026-04-15 19:24:10 +0530

    dir fix

--- server.js
+++ server.js
@@ -18,10 +18,10 @@ const scrapeCache = new NodeCache({ stdTTL: 600 });
 
 app.use(cors());
 app.use(express.json({ limit: '10mb' }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'static')));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.sendFile(path.join(__dirname, 'static', 'index.html'));
 });
 
 const limiter = rateLimit({
